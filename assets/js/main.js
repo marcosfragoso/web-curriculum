f7e28f17a2501079ded8d4cf723d136d500fc5f6
Marcos Vinícius Fragoso de Oliveira <marcos.vinicius.fragoso.oliveira@ccc.ufcg.edu.br>
2026-03-26 11:04:30 -0300

    change tech

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+AOS.init({
+    duration: 650,
+    once: true,
+    offset: 60,
+    easing: 'ease-out-cubic',
+});
+
+new Typed('#typed', {
+    strings: [
+        'Desenvolvedor Backend',
+        'QA Engineer',
+        'Estudante de Ciência da Computação',
+        'Especialista em Java & Python',
+    ],
+    typeSpeed: 58,
+    backSpeed: 32,
+    backDelay: 2000,
+    startDelay: 400,
+    loop: true,
+    smartBackspace: true,
+});
